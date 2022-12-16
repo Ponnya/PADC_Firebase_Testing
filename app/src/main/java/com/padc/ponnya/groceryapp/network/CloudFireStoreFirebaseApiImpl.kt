cc5fae0842baba3ea493c7c27e3e4349c23684e6
Ponnya <mr.ponnya@gmail.com>
2022-12-16 17:03:33 +0630

    -Read data complete(Cloud FireStore)
    Signed-off-by: Ponnya <mr.ponnya@gmail.com>

--- app/src/main/java/com/padc/ponnya/groceryapp/network/CloudFireStoreFirebaseApiImpl.kt
+++ app/src/main/java/com/padc/ponnya/groceryapp/network/CloudFireStoreFirebaseApiImpl.kt
@@ -1,14 +1,38 @@
 package com.padc.ponnya.groceryapp.network
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.padc.ponnya.groceryapp.data.vos.GroceryVO
+import com.padc.ponnya.groceryapp.utils.AMOUNT_FIELD
+import com.padc.ponnya.groceryapp.utils.DESCRIPTION_FIELD
+import com.padc.ponnya.groceryapp.utils.NAME_FIELD
 
 
 object CloudFireStoreFirebaseApiImpl : FirebaseApi {
+    private val db = Firebase.firestore
     override fun getGroceries(
         onSuccess: (groceries: List<GroceryVO>) -> Unit,
         onFialure: (String) -> Unit
     ) {
+        db.collection("Groceries")
+            .get()
+            .addOnSuccessListener { result ->
+                val groceryList = arrayListOf<GroceryVO>()
+                for (document in result) {
+                    val data = document.data
+                    var grocery = GroceryVO(
+                        name = data[NAME_FIELD] as String,
+                        description = data[DESCRIPTION_FIELD] as String,
+                        amount = (data[AMOUNT_FIELD] as Long).toInt()
+                    )
+                    groceryList.add(grocery)
 
+                }
+                onSuccess(groceryList)
+            }
+            .addOnFailureListener {
+                onFialure(it.message ?: "Please check connection")
+            }
     }
 
     override fun addGrocery(name: String, description: String, amount: Int) {
---
+++ app/src/main/java/com/padc/ponnya/groceryapp/utils/ConstData.kt
@@ -0,0 +1,5 @@
+package com.padc.ponnya.groceryapp.utils
+
+const val NAME_FIELD = "name"
+const val DESCRIPTION_FIELD = "description"
+const val AMOUNT_FIELD = "amount"
