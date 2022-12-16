ca8e36827d5f774ff9bdb5e1d5150c6831522b87
Ponnya <mr.ponnya@gmail.com>
2022-12-16 17:39:37 +0630

    -Add data complete(Cloud FireStore)
    Signed-off-by: Ponnya <mr.ponnya@gmail.com>

--- app/src/main/java/com/padc/ponnya/groceryapp/network/CloudFireStoreFirebaseApiImpl.kt
+++ app/src/main/java/com/padc/ponnya/groceryapp/network/CloudFireStoreFirebaseApiImpl.kt
@@ -5,6 +5,7 @@ import com.google.firebase.ktx.Firebase
 import com.padc.ponnya.groceryapp.data.vos.GroceryVO
 import com.padc.ponnya.groceryapp.utils.AMOUNT_FIELD
 import com.padc.ponnya.groceryapp.utils.DESCRIPTION_FIELD
+import com.padc.ponnya.groceryapp.utils.GROCERIES_COLLECTION
 import com.padc.ponnya.groceryapp.utils.NAME_FIELD
 
 
@@ -36,7 +37,15 @@ object CloudFireStoreFirebaseApiImpl : FirebaseApi {
     }
 
     override fun addGrocery(name: String, description: String, amount: Int) {
+        val groceryMap = hashMapOf(
+            NAME_FIELD to name,
+            DESCRIPTION_FIELD to description,
+            AMOUNT_FIELD to amount
+        )
 
+        db.collection(GROCERIES_COLLECTION)
+            .document(name)
+            .set(groceryMap)
     }
 
     override fun deleteGrocery(name: String) {
--- app/src/main/java/com/padc/ponnya/groceryapp/utils/ConstData.kt
+++ app/src/main/java/com/padc/ponnya/groceryapp/utils/ConstData.kt
@@ -1,5 +1,6 @@
 package com.padc.ponnya.groceryapp.utils
 
+const val GROCERIES_COLLECTION = "Groceries"
 const val NAME_FIELD = "name"
 const val DESCRIPTION_FIELD = "description"
 const val AMOUNT_FIELD = "amount"
