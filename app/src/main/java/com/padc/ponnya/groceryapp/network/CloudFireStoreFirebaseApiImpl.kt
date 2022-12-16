1ac7f389660c7992fe9f8f00a3c0efac450a4de8
Ponnya <mr.ponnya@gmail.com>
2022-12-16 17:49:46 +0630

    -Realtime observation of data
    Signed-off-by: Ponnya <mr.ponnya@gmail.com>

--- app/src/main/java/com/padc/ponnya/groceryapp/network/CloudFireStoreFirebaseApiImpl.kt
+++ app/src/main/java/com/padc/ponnya/groceryapp/network/CloudFireStoreFirebaseApiImpl.kt
@@ -15,24 +15,43 @@ object CloudFireStoreFirebaseApiImpl : FirebaseApi {
         onSuccess: (groceries: List<GroceryVO>) -> Unit,
         onFialure: (String) -> Unit
     ) {
-        db.collection("Groceries")
-            .get()
-            .addOnSuccessListener { result ->
-                val groceryList = arrayListOf<GroceryVO>()
-                for (document in result) {
-                    val data = document.data
-                    var grocery = GroceryVO(
-                        name = data[NAME_FIELD] as String,
-                        description = data[DESCRIPTION_FIELD] as String,
-                        amount = (data[AMOUNT_FIELD] as Long).toInt()
-                    )
-                    groceryList.add(grocery)
+        /* db.collection(GROCERIES_COLLECTION)
+             .get()
+             .addOnSuccessListener { result ->
+                 val groceryList = arrayListOf<GroceryVO>()
+                 for (document in result) {
+                     val data = document.data
+                     var grocery = GroceryVO(
+                         name = data[NAME_FIELD] as String,
+                         description = data[DESCRIPTION_FIELD] as String,
+                         amount = (data[AMOUNT_FIELD] as Long).toInt()
+                     )
+                     groceryList.add(grocery)
 
+                 }
+                 onSuccess(groceryList)
+             }
+             .addOnFailureListener {
+                 onFialure(it.message ?: "Please check connection")
+             }*/
+
+        db.collection(GROCERIES_COLLECTION)
+            .addSnapshotListener { value, error ->
+                error?.let { onFialure(it.message ?: "Please check connection") } ?: kotlin.run {
+                    val groceryList = arrayListOf<GroceryVO>()
+                    val result = value?.documents ?: listOf()
+
+                    for (document in result) {
+                        val data = document.data ?: mapOf()
+                        val grocery = GroceryVO(
+                            name = data[NAME_FIELD] as String,
+                            description = data[DESCRIPTION_FIELD] as String,
+                            amount = (data[AMOUNT_FIELD] as Long).toInt()
+                        )
+                        groceryList.add(grocery)
+                    }
+                    onSuccess(groceryList)
                 }
-                onSuccess(groceryList)
-            }
-            .addOnFailureListener {
-                onFialure(it.message ?: "Please check connection")
             }
     }
 
